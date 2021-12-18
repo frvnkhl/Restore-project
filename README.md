792b2fe6a710c0d9eacb5ec34cb5f9de64d3ffd3
Frank Uhal <franquito@Franks-Mac-mini.local>
2021-12-18 12:39:19 +0100

    update readme, add client side

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Restore-project
+## Restore-project
+This is my first ASP.NET + React project, done along with Udemy course to learn to create a full-stack e-commerce store from scratch.
+
+# API side of the app
+.NET6, C#
+
+# Client side of the app
+React, JS
+
+# Course of the project
+https://www.udemy.com/course/learn-to-build-an-e-commerce-store-with-dotnet-react-redux/
---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+function App() {
+  const [products, setProduct] = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+  ]);
+
+  function addProduct() {
+    setProduct(prevState => [...prevState,
+    {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+  }
+
+  return (
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
+    </div>
+  );
+}
+
+export default App;
