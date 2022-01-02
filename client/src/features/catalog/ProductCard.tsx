b5d7b7dcfe031149bc981d7624ccb1317a57747d
Frank Uhal <franquito@Franks-Mac-mini.local>
2022-01-02 14:47:08 +0100

    finish react router part

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import { Product } from "../../app/models/product";
 
 interface Props {
@@ -8,19 +9,19 @@ interface Props {
 export default function ProductCard({ product }: Props) {
     return (
         <Card>
-            <CardHeader 
+            <CardHeader
                 avatar={
-                    <Avatar sx={{bgcolor: 'secondary.main'}}>
+                    <Avatar sx={{ bgcolor: 'secondary.main' }}>
                         {product.name.charAt(0).toUpperCase()}
                     </Avatar>
                 }
                 title={product.name}
-                titleTypographyProps = {{
-                    sx: {fontWeight: 'bold', color: 'primary.main'}
+                titleTypographyProps={{
+                    sx: { fontWeight: 'bold', color: 'primary.main' }
                 }}
             />
             <CardMedia
-                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light'}}
+                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
                 image={product.pictureUrl}
                 title={product.name}
             />
@@ -34,7 +35,7 @@ export default function ProductCard({ product }: Props) {
             </CardContent>
             <CardActions>
                 <Button size="small">Add to cart</Button>
-                <Button size="small">View</Button>
+                <Button component={Link} to={`catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
